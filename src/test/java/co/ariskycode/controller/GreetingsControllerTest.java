2ed3e8487286e297a39f49c5df17006c459d1818
Sanman Shelar <39533009+sanman-shelar@users.noreply.github.com>
2019-11-23 14:10:37 +0530

    added junit for /greet endpoint

---
+++ src/test/java/co/ariskycode/controller/GreetingsControllerTest.java
@@ -0,0 +1,46 @@
+package co.ariskycode.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+@ExtendWith(SpringExtension.class)
+@WebFluxTest
+public class GreetingsControllerTest {
+
+	@Autowired
+	WebTestClient webTestClient;
+	
+	@Test
+	@WithMockUser
+	public void greetTestWithValidResponse() {
+		Flux<String> result = webTestClient.get().uri("/greet")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.returnResult(String.class)
+		.getResponseBody();
+		
+		StepVerifier.create(result)
+		.expectSubscription()
+		.expectNext("Hello")
+		.verifyComplete();
+	}
+	
+	@Test
+	public void greetTestWithUnauthorizedResponse() {
+		webTestClient.get().uri("/greet")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()		
+		.expectStatus().isUnauthorized();
+		
+	}
+}
