e7dc3744b13152034d4263b56b27e7c3bd94ec32
Sanman Shelar <39533009+sanman-shelar@users.noreply.github.com>
2019-11-23 11:59:32 +0530

    refactored security bean

--- src/main/java/co/ariskycode/security/ApplicationSecurityConfiguration.java
+++ src/main/java/co/ariskycode/security/ApplicationSecurityConfiguration.java
@@ -2,7 +2,6 @@ package co.ariskycode.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -15,26 +14,26 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.server.WebFilter;
 
 @EnableWebFluxSecurity
-@EnableReactiveMethodSecurity
 public class ApplicationSecurityConfiguration {
 
+	private final static String contextPath = "/secure/controller";
+	
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 		http.addFilterBefore(contextPathWebFilter(), SecurityWebFiltersOrder.FIRST)
 		.csrf().disable()
 		.authorizeExchange()
-		.pathMatchers("/secure/controller/greet").hasRole("USER")
-		.pathMatchers("/secure/controller/admin").hasRole("ADMIN")
-		.and()
-        .formLogin()
+		.pathMatchers(contextPath + "/greet").hasRole("USER")
+		.pathMatchers(contextPath + "/admin").hasRole("ADMIN")
+		.anyExchange()
+        .authenticated()
 		.and()
 		.httpBasic();
 		return http.build();
 	}
 	
 	@Bean
-	public WebFilter contextPathWebFilter() {
-	    String contextPath = "/secure/controller";
+	public WebFilter contextPathWebFilter() {	    
 	    return (exchange, chain) -> {
 	        ServerHttpRequest request = exchange.getRequest();
 	        if (request.getURI().getPath().startsWith(contextPath)) {
