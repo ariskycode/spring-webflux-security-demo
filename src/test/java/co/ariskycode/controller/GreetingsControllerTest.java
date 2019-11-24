1aa60f48bc42c30f79042cabb9e4f5b1f1556704
Sanman Shelar <39533009+sanman-shelar@users.noreply.github.com>
2019-11-24 15:37:06 +0530

    added rest docs

--- src/test/java/co/ariskycode/controller/GreetingsControllerTest.java
+++ src/test/java/co/ariskycode/controller/GreetingsControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithAnonymousUser;
@@ -14,11 +15,15 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+
 @WebFluxTest
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 public class GreetingsControllerTest {
 
 	@Autowired
 	WebTestClient webTestClient;
+	
 	
 	@DisplayName("Should greet the user")
 	@ParameterizedTest(name = "having name as {0} with Hello, {0}")
@@ -29,14 +34,48 @@ public class GreetingsControllerTest {
 		.uri(uriBuilder -> uriBuilder.path("/greet").queryParam("name", name).build())
 		.accept(MediaType.APPLICATION_JSON)
 		.exchange()
-		.expectStatus().isOk()
-		.returnResult(String.class)
+		.expectStatus().isOk()		
+		.returnResult(String.class)		
 		.getResponseBody();
 		
 		StepVerifier.create(result)
 		.expectSubscription()
 		.expectNext("Hello, ".concat(name))
 		.verifyComplete();
+	}
+	
+	@DisplayName("Should greet the user with Hello, user")	
+	@WithMockUser
+	@Test
+	public void greetTestWithNoParamsReturnsValidResponse() {
+		 Flux<String> result = webTestClient.get()
+		.uri("/greet")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.returnResult(String.class)		
+		.getResponseBody();
+		
+		
+		 StepVerifier.create(result) 
+		 .expectSubscription() 
+		 .expectNext("Hello, user")
+		 .verifyComplete();
+		 
+	}
+	
+
+	@WithMockUser
+	@Test
+	public void greetTestWithNoParamsShouldReturnBody() {
+		webTestClient.get()
+		.uri("/greet")
+		.accept(MediaType.APPLICATION_JSON)
+		.exchange()
+		.expectStatus().isOk()
+		.expectBody()
+		.consumeWith(document("greet"));
+		
 	}
 	
 	@Test
